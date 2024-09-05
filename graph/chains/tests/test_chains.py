5035ea09c7589119417b978a0448bdbba4e35d78
Stephane Remigereau <sremigere@gmail.com>
2024-09-04 18:08:01 -0700

    grade node 2
    Signed-off-by: Stephane Remigereau <sremigere@gmail.com>

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,2 +1,40 @@
-def test_foo():
-    assert True
+from pprint import pprint
+
+
+from typing import List
+from dotenv import load_dotenv
+from langchain_core.documents.base import Document
+
+load_dotenv()
+
+from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+
+from ingestion import retriever
+
+# def test_generation_chain() -> None:
+#     question = "agent memory"
+#     docs = retriever.invoke(question)
+#     generation = generation_chain.invoke({"context": docs, "question": question})
+#     pprint(generation)
+    
+def test_retrival_grade_answer_yes():
+    question = "agent memory"
+    docs: List[Document] = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": question, "document": doc_txt}
+    )
+    assert res.binary_score == "yes"
+    
+    
+def test_retrival_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_txt = docs[1].page_content
+
+    res: GradeDocuments = retrieval_grader.invoke(
+        {"question": "how to make pizaa", "document": doc_txt}
+    )
+
+    assert res.binary_score == "no"
+    
--- ingestion.py
+++ ingestion.py
@@ -1,39 +1,50 @@
+import os
 from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import WebBaseLoader
 from langchain_chroma import Chroma
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.vectorstores.base import VectorStoreRetriever
 from langchain_openai import OpenAIEmbeddings
-from regex import W
 
 load_dotenv()
 
+# Define persistence directory
+persist_directory = "./.chroma"
+
+# URLs to be processed
 urls = [
     "https://lilianweng.github.io/posts/2023-06-23-agent/",
     "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
     "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
 ]
 
+# Load the documents from the URLs
 docs = [WebBaseLoader(url).load() for url in urls]
-
 docs_list = [item for sublist in docs for item in sublist]
 
+# Split the documents into chunks
 text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-    chunk_size=250,
-    chunk_overlap=0,
+    chunk_size=250, chunk_overlap=0
 )
-
 doc_splits = text_splitter.split_documents(docs_list)
 
-vectorstore = Chroma().from_documents(
-    documents=doc_splits,
-    collection_name="rag-chroma",
-    embedding=OpenAIEmbeddings(),
-    persist_directory="./.chroma",
-)
+# Check if the Chroma vector store already exists
+if not os.path.exists(persist_directory):
+    print("Chroma vector store not found. Creating a new one...")
 
-retriever = Chroma(
+    # Initialize Chroma and insert the documents if the store doesn't exist
+    vectorstore = Chroma.from_documents(
+        documents=doc_splits,
+        collection_name="rag-chroma",
+        embedding=OpenAIEmbeddings(),
+        persist_directory=persist_directory,
+    )
+else:
+    print("Chroma vector store already exists. Skipping document insertion.")
+
+# Load the retriever from the Chroma store (existing or newly created)
+retriever: VectorStoreRetriever = Chroma(
     collection_name="rag-chroma",
-    persist_directory="./.chroma",
+    persist_directory=persist_directory,
     embedding_function=OpenAIEmbeddings(),
 ).as_retriever()
-
